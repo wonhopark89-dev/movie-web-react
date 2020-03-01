dc1ce1c7e3d8d0d8222bfadab1677e04ae064030
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-03-01 22:43:50 +0900

    add hook sample

--- src/Hook.js
+++ src/Hook.js
@@ -1,5 +1,5 @@
 
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component, useState, useEffect, useRef, createRef } from 'react';
 import Axios from 'axios';
 
 // class App extends Component {
@@ -93,23 +93,65 @@ function useFetch(url) {
     return { payload, loading, error };
 }
 
+// function App() {
+//     const name = useInput("");
+//     const { payload, loading, error } = useFetch("http://aws.random.cat/meow");
+//     return (
+
+//         <div className="App">
+//             <h1>use Hook</h1>
+//             <br />
+//             <input {...name} placeholder="what your name"></input>
+//             {/* <input value={name.value} onChange={name.onChange} placeholder="what your name 2"></input> */}
+
+//             <br />
+//             {loading && <span>loading your cat</span>}
+//             {!loading && error && <span>{error}</span>}
+//             {!loading && payload && <img src={payload.file} width="150"></img>}
+//         </div>
+
+//     );
+// }
+
+
+function clickOutside(fn) {
+    useEffect(() => {
+        document.addEventListener("click", handleClick);
+    }, []);
+    
+    const ref = createRef();
+    const handleClick = e => {
+        console.log(e.target);
+        if(!ref.current.contains(e.target)) {
+            fn();
+        }
+    };
+
+
+ 
+    return ref;
+}
+
 function App() {
-    const name = useInput("");
-    const { payload, loading, error } = useFetch("http://aws.random.cat/meow");
+
+    const onClickOutside = () => {
+        console.log("lalala");
+    }
+    // const inputRef = useRef();
+    const refs = clickOutside(onClickOutside);
+
+    // setTimeout(() => {
+    //     inputRef.current.focus(); // 5초후에 해당 엘리먼트 포커싱
+    // }, 5000);
+
     return (
-
         <div className="App">
-            <h1>use Hook</h1>
-            <br />
-            <input {...name} placeholder="what your name"></input>
-            {/* <input value={name.value} onChange={name.onChange} placeholder="what your name 2"></input> */}
-
-            <br />
-            {loading && <span>loading your cat</span>}
-            {!loading && error && <span>{error}</span>}
-            {!loading && payload && <img src={payload.file} width="150"></img>}
+            <div ref={refs}>
+                <h1>this should be popup</h1>
+                <h2>ppppopopopop</h2>
+                <input type="text"></input>
+            </div>
         </div>
-
     );
 }
 
