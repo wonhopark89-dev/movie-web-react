71cd524416938a0703dc8aefdfeace98aeaa4fe6
Wonho Park <wonho.park@ybrain.com>
2020-03-14 16:14:40 +0900

    add sample

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,14 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Moive({ id, year, title, summary, poster }) {
     return (
+        <Link to={{
+                pathname:`/moive/${id}`,
+                state : {id, year, title, summary, poster}
+        }}>
         <div>
             <h2>{title}</h2>
             <h3>{year}</h3>
             <h5>{summary}</h5>
             <img src={poster} width="150" />
         </div>
+        </Link>
     );
 }
 
